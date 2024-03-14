28afef4a71e2663a8df5c259dbba64f1aebc9abf
saiprasad-hydraboiny <prasadhydraboiny9122@gmail.com>
2024-03-14 12:41:43 +0530

    Implementing Runnable and Deep diving into Thread Class

--- src/main/java/com/MultiThreading/Main.java
+++ src/main/java/com/MultiThreading/Main.java
@@ -6,6 +6,20 @@ public class Main {
         Thread thread1=new Thread1("thread1");//program will only run if there is only At least one User Thread
 //        thread1.setDaemon(true);//Making a thread a Daemon Thread,Daemon thread has no existence without User Thread
         thread1.start();
+
+        //Implementing Runnable
+        Thread thread2=new Thread(new Thread2());
+        thread2.start();
+
+        //Using LAMDAS
+        Thread thread3=new Thread(()->{
+            for(int i=0;i<5;i++)
+            {
+                System.out.println("inside "+Thread.currentThread()+" "+i);
+            }
+        },"thread3");
+        thread3.start();
+
         System.out.println("Main is Exiting");
     }
 }
---
+++ src/main/java/com/MultiThreading/Thread2.java
@@ -0,0 +1,12 @@
+package com.MultiThreading;
+
+public class Thread2 implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("inside "+Thread.currentThread()+" "+i);
+        }
+    }
+}
